aff73bd0ff29db715a1c84fe86c24bd23bec92fc
moniikag <monikaglier@gmail.com>
2017-01-22 20:22:07 +0100

    test incrementAsync saga

--- sagas.js
+++ sagas.js
@@ -3,10 +3,10 @@ export function* helloSaga() {
 }
 
 import { delay } from 'redux-saga'
-import { put, takeEvery } from 'redux-saga/effects'
+import { put, call, takeEvery } from 'redux-saga/effects'
 
 export function* incrementAsync() {
-  yield delay(1000)
+  yield call(delay, 1000)
   yield put({ type: 'INCREMENT' })
 }
 
---
+++ sagas.spec.js
@@ -0,0 +1,23 @@
+import test from 'tape'
+
+import { put, call } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+import { incrementAsync } from './sagas'
+
+test('incrementAsync Saga test', (assert) => {
+  const gen = incrementAsync()
+
+  assert.deepEqual(
+    gen.next().value,
+    call(delay, 1000),
+    'incrementAsync Saga must call delay(1000)'
+  )
+
+  assert.deepEqual(
+    gen.next().value,
+    put({type: 'INCREMENT'}),
+    'incrementAsync Saga must be done'
+  )
+
+  assert.end()
+})
